6b3084adde7af6943026293e335d09a04ddae71e
borislavski <v.borislavski@gmail.com>
2023-02-25 13:54:51 +0200

    first commitm

---
+++ script.js
@@ -0,0 +1,7 @@
+const firstNum = prompt("Put the first number", 0);
+const secondNum = prompt("Put the second number", 0);
+
+alert("Sum: " + (Number(firstNum) + Number(secondNum)));
+alert("Subtraction: " + (Number(firstNum) - Number(secondNum)));
+alert("Multiplication: " + (Number(firstNum) * Number(secondNum)));
+alert("Division: " + (Number(firstNum) / Number(secondNum)));
